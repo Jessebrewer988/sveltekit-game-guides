c391f8509480d6e84f7bb8cf3676499bf3064279
Jessebrewer988 <Jbrewerwriter@gmail.com>
2022-01-18 12:49:32 -0500

    updated sveltekit version

---
+++ .vercel_build_output/static/_app/pages/index.svelte-e37f302b.js
@@ -0,0 +1,17 @@
+import{S as H,i as I,s as R,e as a,t as d,k as b,c as s,a as r,h as m,d as o,m as _,b as P,g as z,G as e,L as V}from"../chunks/vendor-8ee93b32.js";function B(D){let i,u,f,q,c,g,L,p,M,N,h,x,E,l,y,A,n,G;return{c(){i=a("div"),u=a("h2"),f=d("Welcome"),q=b(),c=a("p"),g=d(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas deleniti dolorum odio
+		sint commodi veniam impedit ea tempora, nobis omnis mollitia aliquid corporis repudiandae? Nulla
+		libero et eius ratione!`),L=b(),p=a("p"),M=d(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas deleniti dolorum odio
+		sint commodi veniam impedit ea tempora, nobis omnis mollitia aliquid corporis repudiandae? Nulla
+		libero et eius ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas
+		deleniti dolorum odio sint commodi veniam impedit ea tempora, nobis omnis mollitia aliquid
+		corporis repudiandae? Nulla libero et eius ratione!`),N=b(),h=a("p"),x=d(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas deleniti dolorum odio
+		sint commodi veniam impedit ea tempora, nobis omnis mollitia aliquid corporis repudiandae? Nulla
+		libero et eius ratione!`),E=b(),l=a("a"),y=d("View Guides"),A=b(),n=a("a"),G=d("About"),this.h()},l(v){i=s(v,"DIV",{class:!0});var t=r(i);u=s(t,"H2",{});var k=r(u);f=m(k,"Welcome"),k.forEach(o),q=_(t),c=s(t,"P",{});var w=r(c);g=m(w,`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas deleniti dolorum odio
+		sint commodi veniam impedit ea tempora, nobis omnis mollitia aliquid corporis repudiandae? Nulla
+		libero et eius ratione!`),w.forEach(o),L=_(t),p=s(t,"P",{});var S=r(p);M=m(S,`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas deleniti dolorum odio
+		sint commodi veniam impedit ea tempora, nobis omnis mollitia aliquid corporis repudiandae? Nulla
+		libero et eius ratione! Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas
+		deleniti dolorum odio sint commodi veniam impedit ea tempora, nobis omnis mollitia aliquid
+		corporis repudiandae? Nulla libero et eius ratione!`),S.forEach(o),N=_(t),h=s(t,"P",{});var W=r(h);x=m(W,`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, quas deleniti dolorum odio
+		sint commodi veniam impedit ea tempora, nobis omnis mollitia aliquid corporis repudiandae? Nulla
+		libero et eius ratione!`),W.forEach(o),E=_(t),l=s(t,"A",{href:!0});var j=r(l);y=m(j,"View Guides"),j.forEach(o),A=_(t),n=s(t,"A",{href:!0});var C=r(n);G=m(C,"About"),C.forEach(o),t.forEach(o),this.h()},h(){P(l,"href","/guides"),P(n,"href","/about"),P(i,"class","index svelte-3gsv4c")},m(v,t){z(v,i,t),e(i,u),e(u,f),e(i,q),e(i,c),e(c,g),e(i,L),e(i,p),e(p,M),e(i,N),e(i,h),e(h,x),e(i,E),e(i,l),e(l,y),e(i,A),e(i,n),e(n,G)},p:V,i:V,o:V,d(v){v&&o(i)}}}class J extends H{constructor(i){super();I(this,i,null,B,R,{})}}export{J as default};
